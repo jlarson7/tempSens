0e5efd063a302f2a71b7a011961f5da1db1c6154
jlarson7 <joshua-j-larson@uiowa.edu>
2018-12-02 11:26:43 -0600

    Update readTemp.js

--- readTemp.js
+++ readTemp.js
@@ -1,7 +1,7 @@
 const sensor = require('ds18b20-raspi');
  
 // round temperature reading to 1 digit
-const tempC = sensor.readSimpleC(1);
+var tempC = sensor.readSimpleC(1);
 console.log(`${tempC} degC`);
  
 // round temperature reading to 1 digit
