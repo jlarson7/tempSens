5b36899da3481264ce2f492895e2342b62c76fc2
jlarson7 <joshua-j-larson@uiowa.edu>
2018-12-02 11:30:05 -0600

    Update readTemp.js

--- readTemp.js
+++ readTemp.js
@@ -1,8 +1,11 @@
 const sensor = require('ds18b20-raspi');
- 
+const delay = require('delay');
 // round temperature reading to 1 digit
-var tempC = sensor.readSimpleC(1);
-console.log(`${tempC} degC`);
+while(1){
+	var tempC = sensor.readSimpleC(1);
+	console.log(`${tempC} degC`);
+	await delay(2000);
+}
  
 // round temperature reading to 1 digit
 sensor.readSimpleC(1, (err, temp) => {
